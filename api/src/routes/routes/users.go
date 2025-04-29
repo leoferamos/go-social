dd0bb8ee3e9367d7a33fdbdf1006940548cbaa66
leoferamos <fernandesramosleonardo@gmail.com>
2025-04-29 15:06:03 -0300

    Refactor user routes to use controller functions for handling requests

--- api/src/routes/routes/users.go
+++ api/src/routes/routes/users.go
@@ -1,46 +1,39 @@
 package routes
 
-import "net/http"
+import (
+	"api/src/controllers"
+	"net/http"
+)
 
 var userRoutes = []Route{
 	{
-		URI:    "users/",
-		Method: http.MethodPost,
-		Function: func(http.ResponseWriter, *http.Request) {
-
-		},
+		URI:          "users/",
+		Method:       http.MethodPost,
+		Function:     controllers.CreateUser,
 		RequiresAuth: false,
 	},
 	{
-		URI:    "users/{userId}",
-		Method: http.MethodGet,
-		Function: func(http.ResponseWriter, *http.Request) {
-
-		},
+		URI:          "users/{userId}",
+		Method:       http.MethodGet,
+		Function:     controllers.GetUser,
 		RequiresAuth: false,
 	},
 	{
-		URI:    "users/",
-		Method: http.MethodGet,
-		Function: func(http.ResponseWriter, *http.Request) {
-
-		},
+		URI:          "users/",
+		Method:       http.MethodGet,
+		Function:     controllers.GetUsers,
 		RequiresAuth: false,
 	},
 	{
-		URI:    "users/{userId}",
-		Method: http.MethodPut,
-		Function: func(http.ResponseWriter, *http.Request) {
-
-		},
+		URI:          "users/{userId}",
+		Method:       http.MethodPut,
+		Function:     controllers.UpdateUser,
 		RequiresAuth: false,
 	},
 	{
-		URI:    "users/{userId}",
-		Method: http.MethodPost,
-		Function: func(http.ResponseWriter, *http.Request) {
-
-		},
+		URI:          "users/{userId}",
+		Method:       http.MethodDelete,
+		Function:     controllers.DeleteUser,
 		RequiresAuth: false,
 	},
 }
