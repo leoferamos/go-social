9c05bf3bcbc41ce2f142c340a2c374e9298fa1a4
leoferamos <fernandesramosleonardo@gmail.com>
2025-04-29 14:44:18 -0300

    Add additional user routes for handling user operations

--- api/src/routes/routes/users.go
+++ api/src/routes/routes/users.go
@@ -11,4 +11,36 @@ var userRoutes = []Route{
 		},
 		RequiresAuth: false,
 	},
+	{
+		URI:    "users/{userId}",
+		Method: http.MethodGet,
+		Function: func(http.ResponseWriter, *http.Request) {
+
+		},
+		RequiresAuth: false,
+	},
+	{
+		URI:    "users/",
+		Method: http.MethodGet,
+		Function: func(http.ResponseWriter, *http.Request) {
+
+		},
+		RequiresAuth: false,
+	},
+	{
+		URI:    "users/{userId}",
+		Method: http.MethodPut,
+		Function: func(http.ResponseWriter, *http.Request) {
+
+		},
+		RequiresAuth: false,
+	},
+	{
+		URI:    "users/{userId}",
+		Method: http.MethodPost,
+		Function: func(http.ResponseWriter, *http.Request) {
+
+		},
+		RequiresAuth: false,
+	},
 }
