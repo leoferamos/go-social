83843d47e19c14091b944158b18136633625f77c
leoferamos <fernandesramosleonardo@gmail.com>
2025-04-29 14:43:47 -0300

    Add user management functions for Create, Get, Update, and Delete operations

---
+++ api/src/controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+//CreateUser creates a new user in the database
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+// GetUser retrieves a user from the database by ID
+func GetUser(w http.ResponseWriter, r *http.Request) {
+}
+
+// GetUsers retrieves all users from the database
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+}
+
+// UpdateUser updates an existing user in the database
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+// DeleteUser deletes a user from the database by ID
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+}
