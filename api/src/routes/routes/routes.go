cef8bc6a2edcaba98d664094fac88164526bcd7b
leoferamos <fernandesramosleonardo@gmail.com>
2025-04-29 15:23:56 -0300

    Organize imports and enhance user route configuration function

--- api/src/routes/routes/routes.go
+++ api/src/routes/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represents an API route.
 type Route struct {
@@ -8,4 +12,12 @@ type Route struct {
 	Method       string
 	Function     func(http.ResponseWriter, *http.Request)
 	RequiresAuth bool
+}
+
+// userRoutes contains all user-related routes.
+func Configure(r *mux.Router) *mux.Router {
+	for _, route := range userRoutes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+	return r
 }
