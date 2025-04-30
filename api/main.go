63083bf469238be038342acb211746efeaaa4b49
leoferamos <fernandesramosleonardo@gmail.com>
2025-04-30 15:46:41 -0300

    Add log statement to indicate API startup

--- api/main.go
+++ api/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"api/src/routes"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Starting API...")
 	r := routes.Generate()
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
