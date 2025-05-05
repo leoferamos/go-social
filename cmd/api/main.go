2dd16f462b45d6cfa82ce4af3e520a0f7570f23c
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-05 15:23:23 -0300

    Enhance logging in main function to provide clearer server startup messages

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,7 +11,10 @@ import (
 func main() {
 
 	config.Init()
-	fmt.Println(config.Port)
+	fmt.Println("Starting server on port:", config.Port)
+
 	r := routes.Generate()
+	fmt.Println("Router generated successfully")
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
