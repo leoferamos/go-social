7df5adc27945914c60e23106fb70e30908bb35ed
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-05 15:24:03 -0300

    Implement CreateUser function to handle user creation and response

--- internal/controllers/users.go
+++ internal/controllers/users.go
@@ -1,11 +1,37 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"go_social/internal/db"
+	"go_social/internal/models"
+	"go_social/internal/repositories"
+	"io"
+	"log"
 	"net/http"
 )
 
 // CreateUser creates a new user in the database
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	bodyRequest, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var usuario models.User
+	if err = json.Unmarshal(bodyRequest, &usuario); err != nil {
+		log.Fatal(err)
+	}
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUsersRepository(db)
+	userId, err := repository.CreateUser(usuario)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(fmt.Sprintf("User created successfully with ID: %d", userId)))
 }
 
 // GetUser retrieves a user from the database by ID
