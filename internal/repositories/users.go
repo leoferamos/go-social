6774bac39475ddbc9cd6e617f52e47c4157f6990
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-06 14:47:32 -0300

    Refactor CreateUser and GetUsers functions for improved readability and consistency

--- internal/repositories/users.go
+++ internal/repositories/users.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
 	"go_social/internal/models"
 )
 
@@ -15,8 +16,8 @@ func NewUsersRepository(db *sql.DB) *users {
 }
 
 // CreateUser creates a new user in the database
-func (u *users) CreateUser(user models.User) (uint64, error) {
-	statement, err := u.db.Prepare("INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?)")
+func (repository *users) CreateUser(user models.User) (uint64, error) {
+	statement, err := repository.db.Prepare("INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		return 0, err
 	}
@@ -32,4 +33,30 @@ func (u *users) CreateUser(user models.User) (uint64, error) {
 		return 0, err
 	}
 	return uint64(lastID), nil
+}
+
+// GetUsers retrieves users from the database based on the provided name or username
+func (repository *users) GetUsers(nameOrUsername string) ([]models.User, error) {
+	nameOrUsername = fmt.Sprintf("%%%s%%", nameOrUsername)
+
+	rows, err := repository.db.Query(
+		"SELECT id, name, username, email, created_at FROM users WHERE name LIKE ? OR username LIKE ?",
+		nameOrUsername, nameOrUsername,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Username, &user.Email, &user.CreatedAt); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
 }
