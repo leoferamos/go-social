8453def7781d17b08c57298fe1031f7da15c1fef
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-09 15:57:55 -0300

    Add token validation and extraction functions for JWT handling

--- internal/auth/token.go
+++ internal/auth/token.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	"go_social/config"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,4 +18,33 @@ func CreateToken(userID int) (string, error) {
 	claims["userID"] = userID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+// ValidateToken checks if the provided token is valid.
+func ValidateToken(r *http.Request) error {
+	tokenString := extractToken(r)
+	token, err := jwt.Parse(tokenString, returnKeyFunc)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); ok && token.Valid {
+		return nil
+	}
+	return fmt.Errorf("invalid token")
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func returnKeyFunc(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	}
+	return []byte(config.SecretKey), nil
 }
