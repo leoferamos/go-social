bf4feedbcbefdd1d4f6a5aeb0721f7b8f61d4350
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-12 13:28:36 -0300

    Refactor ValidateToken and ExtractUserID functions for consistency and clarity

--- internal/auth/token.go
+++ internal/auth/token.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go_social/config"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -28,10 +29,31 @@ func ValidateToken(r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	if _, ok := token.Claims.(jwt.Claims); ok && token.Valid {
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		return nil
 	}
-	return errors.New("Invalid token")
+	return errors.New("invalid token")
+}
+
+// ExtractUserID extracts the user ID from the token.
+func ExtractUserID(r *http.Request) (uint64, error) {
+	tokenString := extractToken(r)
+	token, err := jwt.Parse(tokenString, returnKeyFunc)
+	if err != nil {
+		return 0, err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userIDFloat, ok := claims["userID"].(float64)
+		if !ok {
+			return 0, errors.New("userID is not a valid number")
+		}
+		userID, err := strconv.ParseUint(fmt.Sprintf("%.0f", userIDFloat), 10, 64)
+		if err != nil {
+			return 0, err
+		}
+		return userID, nil
+	}
+	return 0, errors.New("invalid token")
 }
 
 func extractToken(r *http.Request) string {
@@ -45,7 +67,7 @@ func extractToken(r *http.Request) string {
 
 func returnKeyFunc(token *jwt.Token) (interface{}, error) {
 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 	}
 	return []byte(config.SecretKey), nil
 }
