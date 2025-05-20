46ff6517cabef7d7f908a18f9b0ad8d5b10d34cc
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-20 11:07:56 -0300

    Implement LikePost function to allow users to like a post

--- internal/controllers/posts.go
+++ internal/controllers/posts.go
@@ -210,3 +210,25 @@ func GetUserPosts(w http.ResponseWriter, r *http.Request) {
 	}
 	responses.JSON(w, http.StatusOK, posts)
 }
+
+// LikePost Likes a post.
+func LikePost(w http.ResponseWriter, r *http.Request) {
+	parameters := mux.Vars(r)
+	postID, err := strconv.ParseUint(parameters["id"], 10, 64)
+	if err != nil {
+		responses.JSONError(w, http.StatusBadRequest, err)
+		return
+	}
+	db, err := db.Connect()
+	if err != nil {
+		responses.JSONError(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+	repository := repositories.NewPostsRepository(db)
+	if err = repository.LikePost(postID); err != nil {
+		responses.JSONError(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusNoContent, nil)
+}
