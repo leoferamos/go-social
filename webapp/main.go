5cbb42952e2a3db07b1389ea9dc0b5a2aa78538d
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-21 14:45:42 -0300

    Add template loading functionality in main application

--- webapp/main.go
+++ webapp/main.go
@@ -5,10 +5,13 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/routes"
+	"webapp/src/utils"
 )
 
 func main() {
 	fmt.Print("Running web application...")
+	utils.LoadTemplates()
+
 	r := routes.Generate()
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
