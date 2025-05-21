a582143364d7a217c4d7b52914f11f1fc912b963
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-21 14:12:36 -0300

    Add route configuration and structure for web application

---
+++ webapp/src/routes/routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Route represents an web application route.
+type Route struct {
+	URI          string
+	Method       string
+	Function     func(http.ResponseWriter, *http.Request)
+	RequiresAuth bool
+}
+
+// Configure sets up the routes for the web application.
+func Configure(r *mux.Router) *mux.Router {
+	routes := loginRoute
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+
+	}
+
+	return r
+}
