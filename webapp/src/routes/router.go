51d9d1c87b52f445a22338e6085844d00d51cbf6
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-21 14:12:21 -0300

    Add router generation function to initialize application routes

---
+++ webapp/src/routes/router.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+)
+
+// Generate returns a router with all application routes registered.
+func Generate() *mux.Router {
+	return mux.NewRouter()
+}
