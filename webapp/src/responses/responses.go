0952873e84ec04a38f2a6895d5723a2da0665067
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-22 15:45:05 -0300

    Add ErrorAPI struct and HandleStatusCode function for error handling

--- webapp/src/responses/responses.go
+++ webapp/src/responses/responses.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+// ErrorAPI represents a standard error response.
+type ErrorAPI struct {
+	Error string `json:"error"`
+}
+
 // JSON returns a JSON response to the client with the specified status code and data.
 func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
@@ -14,4 +19,11 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// HandleStatusCode is a helper function to handle different status codes and return appropriate responses.
+func HandleStatusCode(w http.ResponseWriter, r *http.Response) {
+	var err ErrorAPI
+	json.NewDecoder(r.Body).Decode(&err)
+	JSON(w, r.StatusCode, err)
 }
