c399f3ed44394910adc133995ebb426b16e9f532
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-22 11:48:10 -0300

    Add CreateUser function for user registration handling

---
+++ webapp/src/controllers/users.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// CreateUser handles user registration.
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	user, err := json.Marshal(map[string]string{
+		"name":     r.FormValue("name"),
+		"username": r.FormValue("username"),
+		"email":    r.FormValue("email"),
+		"password": r.FormValue("password"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	response, err := http.Post("http://localhost:8080/users", "application/json", bytes.NewBuffer(user))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+}
