8d1bb49d705756dd484b5d38c8cd5738c817e474
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-22 22:23:14 -0300

    Implement user login handler with JSON request and response

---
+++ webapp/src/controllers/login.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"webapp/src/responses"
+)
+
+// Login handles user login.
+func Login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	user, err := json.Marshal(map[string]string{
+		"username": r.FormValue("username"),
+		"password": r.FormValue("password"),
+	})
+	if err != nil {
+		responses.JSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid user data"})
+		return
+	}
+	response, err := http.Post("http://localhost:8080/login", "application/json", bytes.NewBuffer(user))
+	if err != nil {
+		responses.JSON(w, http.StatusInternalServerError, map[string]string{"error": "Failed to create user"})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		responses.HandleStatusCode(w, response)
+		return
+	}
+
+	responses.JSON(w, response.StatusCode, nil)
+}
