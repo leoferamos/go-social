6814bb10322fbfb46aa9d669499d2a209428c17f
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-23 13:27:16 -0300

    Refactor login event handler to use inline function and update data field names

--- webapp/assets/js/login.js
+++ webapp/assets/js/login.js
@@ -1,19 +1,16 @@
-$(`#login`).on('submit', login);
-
-function login(event) {
+$('#login').on('submit', function(event) {
     event.preventDefault();
-    
+
     $.ajax({
         url: '/login',
         method: 'POST',
         data: {
-            username: $('#username').val(),
+            identifier: $('#identifier').val(),
             password: $('#password').val()
         }
     }).done(function() {
         window.location.href = '/home';
-    }
-    ).fail(function() {
-        alert('Login failed. Please check your username and password.');
+    }).fail(function(xhr) {
+        alert('Login failed: ' + xhr.responseText);
     });
-}
+});
