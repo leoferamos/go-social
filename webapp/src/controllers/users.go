9953f7759bcfd18943d15e6f3334fa4f42b589f2
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-23 14:51:43 -0300

    Refactor CreateUser function to decode user data from request body and fix JSON marshaling

--- webapp/src/controllers/users.go
+++ webapp/src/controllers/users.go
@@ -9,30 +9,25 @@ import (
 
 // CreateUser handles user registration.
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	user, err := json.Marshal(map[string]string{
-		"name":     r.FormValue("name"),
-		"username": r.FormValue("username"),
-		"email":    r.FormValue("email"),
-		"password": r.FormValue("password"),
-	})
+	var user map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		responses.JSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid user data"})
+		return
+	}
+	userJSON, err := json.Marshal(user)
 	if err != nil {
 		responses.JSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid user data"})
 		return
 	}
-	response, err := http.Post("http://localhost:5000/users", "application/json", bytes.NewBuffer(user))
+	response, err := http.Post("http://localhost:5000/users", "application/json", bytes.NewBuffer(userJSON))
 	if err != nil {
 		responses.JSON(w, http.StatusInternalServerError, map[string]string{"error": "Failed to create user"})
 		return
 	}
-
 	defer response.Body.Close()
-
 	if response.StatusCode >= 400 {
 		responses.HandleStatusCode(w, response)
 		return
 	}
-
 	responses.JSON(w, response.StatusCode, nil)
 }
