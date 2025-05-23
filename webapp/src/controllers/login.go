2d1ada0d1ecbaa8175aacc37d67b42c8366c24cb
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-23 14:22:43 -0300

    Update login handler to use 'identifier' for username or email

--- webapp/src/controllers/login.go
+++ webapp/src/controllers/login.go
@@ -12,14 +12,14 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	user, err := json.Marshal(map[string]string{
-		"username": r.FormValue("username"),
-		"password": r.FormValue("password"),
+		"identifier": r.FormValue("identifier"),
+		"password":   r.FormValue("password"),
 	})
 	if err != nil {
 		responses.JSON(w, http.StatusBadRequest, responses.ErrorAPI{Error: err.Error()})
 		return
 	}
-	response, err := http.Post("http://localhost:8080/login", "application/json", bytes.NewBuffer(user))
+	response, err := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(user))
 	if err != nil {
 		responses.JSON(w, http.StatusInternalServerError, responses.ErrorAPI{Error: err.Error()})
 		return
