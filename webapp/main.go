cf3c7135572159f811d95b9cc358d79412c6b51f
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-24 11:48:22 -0300

    feat(main): load port from environment variable for configurable server startup

--- webapp/main.go
+++ webapp/main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"webapp/src/config"
 	"webapp/src/routes"
 	"webapp/src/utils"
 )
 
 func main() {
+	config.Load()
 	fmt.Print("Running web application...")
 	utils.LoadTemplates()
 
 	r := routes.Generate()
-	log.Fatal(http.ListenAndServe(":8080", r))
+
+	port := os.Getenv("WEBAPP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
