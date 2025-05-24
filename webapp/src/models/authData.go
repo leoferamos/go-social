a476246dd041d70105940f122fc7007ee5a9ac19
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-24 12:11:15 -0300

    refactor(models): change AuthData ID type from uint to string

--- webapp/src/models/authData.go
+++ webapp/src/models/authData.go
@@ -2,6 +2,6 @@ package models
 
 // AuthData represents the authentication data for a user
 type AuthData struct {
-	ID    uint64 `json:"id"`
+	ID    string `json:"id"`
 	Token string `json:"token"`
 }
