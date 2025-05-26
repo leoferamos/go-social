e6f04678be3d124da444b5604ced8de98e137821
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-26 15:12:15 -0300

    feat(middlewares): add logger and authentication middleware functions

---
+++ webapp/src/middlewares/middlewares.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"webapp/src/cookies"
+)
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
+
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if _, err := cookies.Read(r); err != nil {
+			http.Redirect(w, r, "/login", 302)
+			return
+		}
+		next(w, r)
+	}
+}
