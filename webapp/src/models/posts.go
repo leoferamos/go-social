74cafd40f40e829712238ec8035b7d62c9e124b6
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-27 11:26:47 -0300

    feat(posts): add Posts model to represent a post in the application

---
+++ webapp/src/models/posts.go
@@ -0,0 +1,12 @@
+package models
+
+// Posts represents a post in the application.
+type Posts struct {
+	ID             uint64 `json:"id,omitempty"`
+	Title          string `json:"title,omitempty"`
+	Content        string `json:"content,omitempty"`
+	AuthorID       uint64 `json:"author_id,omitempty"`
+	AuthorUsername string `json:"author_username,omitempty"`
+	Likes          uint64 `json:"likes"`
+	CreatedAt      string `json:"created_at,omitempty"`
+}
