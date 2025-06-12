e004d62ba66e2d967081486d7f3b62ccc6253503
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-12 11:10:02 -0300

    refactor(posts): remove title field from Posts model

--- webapp/src/models/posts.go
+++ webapp/src/models/posts.go
@@ -3,7 +3,6 @@ package models
 // Posts represents a post in the application.
 type Posts struct {
 	ID             uint64 `json:"id,omitempty"`
-	Title          string `json:"title,omitempty"`
 	Content        string `json:"content,omitempty"`
 	AuthorID       uint64 `json:"author_id,omitempty"`
 	AuthorUsername string `json:"author_username,omitempty"`
