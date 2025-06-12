bace1e43ba30fbd6116329111959ded83f208951
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-12 11:09:38 -0300

    refactor(posts): remove title field and related validation from Posts model

--- internal/models/posts.go
+++ internal/models/posts.go
@@ -8,7 +8,6 @@ import (
 
 type Posts struct {
 	ID             uint64    `json:"id,omitempty"`
-	Title          string    `json:"title,omitempty"`
 	Content        string    `json:"content,omitempty"`
 	AuthorID       uint64    `json:"author_id,omitempty"`
 	AuthorUsername string    `json:"author_username,omitempty"`
@@ -25,15 +24,11 @@ func (p *Posts) Prepare() error {
 	return nil
 }
 func (p *Posts) validate() error {
-	if p.Title == "" {
-		return errors.New("title is required")
-	}
 	if p.Content == "" {
 		return errors.New("content is required")
 	}
 	return nil
 }
 func (p *Posts) format() {
-	p.Title = strings.TrimSpace(p.Title)
 	p.Content = strings.TrimSpace(p.Content)
 }
