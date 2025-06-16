47e1cdf1168956805f266a0a19993d2ddc4ffbe1
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-16 11:19:07 -0300

    fix(posts): sanitize post content and improve error handling

--- webapp/assets/js/posts.js
+++ webapp/assets/js/posts.js
@@ -16,11 +16,11 @@ $(document).ready(function() {
                     <div class="profile-pic-placeholder"></div>
                     <div class="post-content-area">
                         <header class="post-header">
-                            <span class="post-author-name">${post.author_username}</span>
-                            <span class="post-author-username">@${post.author_username}</span>
-                            <span class="post-time" data-time="${post.created_at}" title=""></span>
+                            <span class="post-author-name">${escapeHtml(post.author_username)}</span>
+                            <span class="post-author-username">@${escapeHtml(post.author_username)}</span>
+                            <span class="post-time" data-time="${escapeHtml(post.created_at)}" title=""></span>
                         </header>
-                        <p class="post-text">${post.content}</p>
+                        <p class="post-text">${escapeHtml(post.content)}</p>
                         <footer class="post-actions">
                             <div><i class="bi bi-heart"></i> <span>${post.likes}</span></div>
                         </footer>
@@ -35,9 +35,31 @@ $(document).ready(function() {
                     }
                 });
             },
-            error: function() {
-                alert('Error creating post. Please try again.');
+            error: function(xhr) {
+                let msg = 'Error creating post. Please try again.';
+                if (xhr && xhr.responseText) {
+                    try {
+                        const resp = JSON.parse(xhr.responseText);
+                        if (resp && resp.error) {
+                            msg = 'Erro: ' + resp.error;
+                        }
+                    } catch (e) {
+                        msg = 'HTTP ' + xhr.status + ': ' + xhr.statusText + '\n' + xhr.responseText;
+                    }
+                } else if (xhr && xhr.status) {
+                    msg = 'HTTP ' + xhr.status + ': ' + xhr.statusText;
+                }
+                alert(msg);
             }
         });
+    }
+
+    function escapeHtml(text) {
+        return text
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
     }
 });
