a5baeb0c12d7b01fc75bd61b36a66c8094132ada
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-17 15:49:47 -0300

    feat(logout): add logout route with authentication requirement

---
+++ webapp/src/routes/logout.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"net/http"
+	"webapp/src/controllers"
+)
+
+var logoutRoute = []Route{
+	{
+		URI:          "/logout",
+		Method:       http.MethodGet,
+		Function:     controllers.Logout,
+		RequiresAuth: true,
+	},
+}
