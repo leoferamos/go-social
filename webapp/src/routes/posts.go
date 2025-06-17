de006673d4131667d8fc2dd597ef0ae296ff3547
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-17 15:07:42 -0300

    feat(posts): add routes for liking and unliking posts with authentication

--- webapp/src/routes/posts.go
+++ webapp/src/routes/posts.go
@@ -12,4 +12,16 @@ var postsRoute = []Route{
 		Function:     controllers.CreatePost,
 		RequiresAuth: true,
 	},
+	{
+		URI:          "/posts/{id}/like",
+		Method:       http.MethodPost,
+		Function:     controllers.LikePost,
+		RequiresAuth: true,
+	},
+	{
+		URI:          "/posts/{id}/unlike",
+		Method:       http.MethodPost,
+		Function:     controllers.UnlikePost,
+		RequiresAuth: true,
+	},
 }
