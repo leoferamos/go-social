dd343a4c4dea41b81968c1a16d5f098cd2d4f872
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-17 13:06:09 -0300

    fix(templates): simplify LoadTemplates function by removing unnecessary template.New call

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -9,7 +9,8 @@ var templates *template.Template
 
 // LoadTemplates loads all HTML templates from the views directory.
 func LoadTemplates() {
-	templates = template.Must(template.New("").ParseGlob("views/*.html"))
+	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/partials/*.html"))
 }
 
 // ExecuteTemplate executes the specified template with the provided data.
