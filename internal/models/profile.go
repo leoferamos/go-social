921c6875ca9a5b0b4267b41aa9e298be0d5149ac
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-25 15:35:39 -0300

    feat(profile): replace User type with PublicUser for enhanced privacy in profile model

--- internal/models/profile.go
+++ internal/models/profile.go
@@ -1,10 +1,16 @@
 package models
 
+type PublicUser struct {
+	ID        uint64 `json:"id"`
+	Name      string `json:"name"`
+	Username  string `json:"username"`
+	Bio       string `json:"bio"`
+	CreatedAt string `json:"created_at"`
+}
+
 type Profile struct {
-	User      User    `json:"user"`
-	Posts     []Posts `json:"posts"`
-	Bio       string  `json:"bio"`
-	Followers int     `json:"followers"`
-	Following int     `json:"following"`
-	CreatedAt string  `json:"created_at"`
+	User      PublicUser `json:"user"`
+	Posts     []Posts    `json:"posts"`
+	Followers int        `json:"followers"`
+	Following int        `json:"following"`
 }
