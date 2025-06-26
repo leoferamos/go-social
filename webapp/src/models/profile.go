96bd878ec4c3d88394f65a7f02a2bc0c3770aba0
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-26 16:01:31 -0300

    refactor(profile): refactor User type in Profile to use PublicUser struct

--- webapp/src/models/profile.go
+++ webapp/src/models/profile.go
@@ -1,10 +1,18 @@
 package models
 
+// PublicUser represents a public view of a user's profile.
+type PublicUser struct {
+	ID        uint64 `json:"id"`
+	Name      string `json:"name"`
+	Username  string `json:"username"`
+	Bio       string `json:"bio"`
+	CreatedAt string `json:"created_at"`
+}
+
+// Profile represents a user's profile in the application.
 type Profile struct {
-	User      map[string]interface{} `json:"user"`
-	Posts     []Post                 `json:"posts"`
-	Bio       string                 `json:"bio"`
-	Followers int                    `json:"followers"`
-	Following int                    `json:"following"`
-	CreatedAt string                 `json:"created_at"`
+	User      PublicUser `json:"user"`
+	Posts     []Post     `json:"posts"`
+	Followers int        `json:"followers"`
+	Following int        `json:"following"`
 }
