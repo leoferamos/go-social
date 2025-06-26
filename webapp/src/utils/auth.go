ed744f63ef3029d916155ec7c3d94412e9a2e1fd
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-26 14:27:38 -0300

    feat(auth): implement GetLoggedUsername function for retrieving logged-in user's username

--- webapp/src/utils/auth.go
+++ webapp/src/utils/auth.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"webapp/src/cookies"
+	"webapp/src/requests"
 )
 
 // IsAuthenticated checks if the request has a valid auth_data cookie with a non-empty token.
@@ -13,4 +16,20 @@ func IsAuthenticated(r *http.Request) bool {
 	}
 	token, ok := cookie["token"]
 	return ok && token != ""
+}
+
+func GetLoggedUsername(r *http.Request, apiURL string, userID uint64) (string, error) {
+	userURL := fmt.Sprintf("%s/users/%d", apiURL, userID)
+	userResp, err := requests.MakeAuthenticatedRequest(r, http.MethodGet, userURL, nil)
+	if err != nil || userResp.StatusCode >= 400 {
+		return "", err
+	}
+	defer userResp.Body.Close()
+	var userData struct {
+		Username string `json:"username"`
+	}
+	if err := json.NewDecoder(userResp.Body).Decode(&userData); err != nil {
+		return "", err
+	}
+	return userData.Username, nil
 }
