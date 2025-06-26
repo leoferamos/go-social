67318b20b7dfb259effc6609d4e6a8ba9db0cc24
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-26 13:58:25 -0300

    feat(posts): add AuthorName field to Posts struct for enhanced post metadata

--- internal/models/posts.go
+++ internal/models/posts.go
@@ -11,6 +11,7 @@ type Posts struct {
 	Content        string    `json:"content,omitempty"`
 	AuthorID       uint64    `json:"author_id,omitempty"`
 	AuthorUsername string    `json:"author_username,omitempty"`
+	AuthorName     string    `json:"author_name,omitempty"`
 	Likes          uint64    `json:"likes"`
 	CreatedAt      time.Time `json:"created_at,omitempty"`
 	LikedByMe      bool      `json:"liked_by_me"`
