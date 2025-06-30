016917b60aa23cd4adba2d71273bcc68cad8828c
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-30 13:22:26 -0300

    feat(users): add follow and unfollow routes for user interaction

--- webapp/src/routes/users.go
+++ webapp/src/routes/users.go
@@ -24,4 +24,16 @@ var userRoutes = []Route{
 		Function:     controllers.LoadProfilePage,
 		RequiresAuth: false,
 	},
+	{
+		URI:          "/users/{userId}/follow",
+		Method:       http.MethodPost,
+		Function:     controllers.FollowUser,
+		RequiresAuth: true,
+	},
+	{
+		URI:          "/users/{userId}/unfollow",
+		Method:       http.MethodPost,
+		Function:     controllers.UnfollowUser,
+		RequiresAuth: true,
+	},
 }
