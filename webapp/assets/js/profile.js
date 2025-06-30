63061e5375ee6fadb6fec2ad2bca6636b7c48c1e
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-30 11:48:24 -0300

    feat(profile): enhance follow button initialization and success handling

--- webapp/assets/js/profile.js
+++ webapp/assets/js/profile.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
-    $('#follow-btn').on('click', follow);
-});
-    function follow() {
-        const $btn = $(this);
-        const userId = $btn.data('user-id');
-        const isFollowing = $btn.hasClass('following');
-
-        $.ajax({
-            url: isFollowing ? `/users/${userId}/unfollow` : `/users/${userId}/follow`,
-            method: 'POST',
-            success: function(data) {
-                if (data.following) {
-                    $btn.addClass('following').text('Unfollow');
-                } else {
-                    $btn.removeClass('following').text('Follow');
-                }
-            },
-            error: function(xhr) {
-                let msg = 'An error occurred while trying to follow/unfollow.';
-                if (xhr.responseJSON && xhr.responseJSON.error) {
-                    msg = xhr.responseJSON.error;
-                }
-                Swal.fire("Oops...", msg, "error");
-            }
-        });
+    const $btn = $('#follow-btn');
+    if ($btn.length) {
+        const isFollowing = Boolean($btn.data('is-following'));
+        if (isFollowing) {
+            $btn
+                .addClass('following btn-outline-primary')
+                .removeClass('btn-primary')
+                .text('Unfollow');
+        } else {
+            $btn
+                .removeClass('following btn-outline-primary')
+                .addClass('btn-primary')
+                .text('Follow');
+        }
     }
+    $btn.on('click', follow);
+});
+
+function follow() {
+    const $btn = $(this);
+    const userId = $btn.data('user-id');
+    const isFollowing = $btn.hasClass('following');
+
+    $.ajax({
+        url: isFollowing ? `/users/${userId}/unfollow` : `/users/${userId}/follow`,
+        method: 'POST',
+        success: function(data) {
+            if (data.following) {
+                $btn
+                    .addClass('following btn-outline-primary')
+                    .removeClass('btn-primary')
+                    .text('Unfollow');
+            } else {
+                $btn
+                    .removeClass('following btn-outline-primary')
+                    .addClass('btn-primary')
+                    .text('Follow');
+            }
+        },
+        error: function(xhr) {
+            let msg = 'An error occurred while trying to follow/unfollow.';
+            if (xhr.responseJSON && xhr.responseJSON.error) {
+                msg = xhr.responseJSON.error;
+            }
+            Swal.fire("Oops...", msg, "error");
+        }
+    });
+}
