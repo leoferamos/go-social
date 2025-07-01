bcaa0d85cf1c8fe2b8a3d7ea8497600c7aa7e7e6
leoferamos <fernandesramosleonardo@gmail.com>
2025-07-01 10:41:20 -0300

    feat(models): add AuthorAvatarURL field to Post struct

--- webapp/src/models/posts.go
+++ webapp/src/models/posts.go
@@ -2,12 +2,13 @@ package models
 
 // Posts represents a post in the application.
 type Post struct {
-	ID             uint64 `json:"id,omitempty"`
-	Content        string `json:"content,omitempty"`
-	AuthorID       uint64 `json:"author_id,omitempty"`
-	AuthorUsername string `json:"author_username,omitempty"`
-	AuthorName     string `json:"author_name,omitempty"`
-	Likes          uint64 `json:"likes"`
-	CreatedAt      string `json:"created_at,omitempty"`
-	LikedByMe      bool   `json:"liked_by_me"`
+	ID              uint64 `json:"id,omitempty"`
+	Content         string `json:"content,omitempty"`
+	AuthorID        uint64 `json:"author_id,omitempty"`
+	AuthorUsername  string `json:"author_username,omitempty"`
+	AuthorName      string `json:"author_name,omitempty"`
+	AuthorAvatarURL string `json:"author_avatar_url,omitempty"`
+	Likes           uint64 `json:"likes"`
+	CreatedAt       string `json:"created_at,omitempty"`
+	LikedByMe       bool   `json:"liked_by_me"`
 }
