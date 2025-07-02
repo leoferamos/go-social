21ddff20979352004900ca635b3de5220828d7fb
leoferamos <fernandesramosleonardo@gmail.com>
2025-07-02 15:37:40 -0300

    feat(models): update User struct to include AvatarURL, BannerURL, and CreatedAt fields

--- webapp/src/models/users.go
+++ webapp/src/models/users.go
@@ -1,10 +1,14 @@
 package models
 
+// User represents a user in the system.
 type User struct {
-	ID       uint64 `json:"id,omitempty"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password,omitempty"`
-	Bio      string `json:"bio,omitempty"`
+	ID        uint64 `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Password  string `json:"password,omitempty"`
+	Bio       string `json:"bio,omitempty"`
+	AvatarURL string `json:"avatar_url,omitempty"`
+	BannerURL string `json:"banner_url,omitempty"`
+	CreatedAt string `json:"created_at,omitempty"`
 }
